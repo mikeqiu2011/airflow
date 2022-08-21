969ff62c4800e96ca1f9b7685e8f231a7f43e5c6
mikeqiu2011 <mikeqiu2011@163.com>
2022-08-21 16:25:40 +0800

    python operator to download

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # airflow-materials
 
 ## test tasks
-
-1. http connection
-   airflow tasks test forex_data_pipeline is_forex_rates_available 2022-01-01
-2. file exists
-   airflow tasks test forex_data_pipeline is_forex_file_available 2022-01-01
+   1. http connection
+      airflow tasks test forex_data_pipeline is_forex_rates_available 2022-01-01
+   2. file exists
+      airflow tasks test forex_data_pipeline is_forex_file_available 2022-01-01
+   3. python operator
+      airflow tasks test forex_data_pipeline is_download_successful 2022-01-01
--- airflow-section-3/mnt/airflow/dags/forex_data_pipeline.py
+++ airflow-section-3/mnt/airflow/dags/forex_data_pipeline.py
@@ -1,7 +1,11 @@
+import csv
 from datetime import datetime, timedelta
+import json
 from airflow import DAG
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.sensors.filesystem import FileSensor
+from airflow.operators.python import PythonOperator
+import requests
 
 default_args = {
     'owner': 'airflow',
@@ -12,17 +16,42 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
+# Download forex rates according to the currencies we want to watch
+# described in the file forex_currencies.csv
+
+
+def download_rates():
+    BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
+    ENDPOINTS = {
+        'USD': 'api_forex_exchange_usd.json',
+        'EUR': 'api_forex_exchange_eur.json'
+    }
+    with open('/opt/airflow/dags/files/forex_currencies.csv') as forex_currencies:
+        reader = csv.DictReader(forex_currencies, delimiter=';')
+        for idx, row in enumerate(reader):
+            base = row['base']
+            with_pairs = row['with_pairs'].split(' ')
+            indata = requests.get(f"{BASE_URL}{ENDPOINTS[base]}").json()
+            outdata = {'base': base, 'rates': {},
+                       'last_update': indata['date']}
+            for pair in with_pairs:
+                outdata['rates'][pair] = indata['rates'][pair]
+            with open('/opt/airflow/dags/files/forex_rates.json', 'a') as outfile:
+                json.dump(outdata, outfile)
+                outfile.write('\n')
+
+
 # id unique across all DAG,
-with DAG('forex_data_pipeline', start_date=datetime(2022, 8, 21),           schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
-    # is_forex_rates_available = HttpSensor(
-    #     task_id='is_forex_rates_available',
-    #     http_conn_id='forex_api',
-    #     # endpoint='marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b',
-    #     endpoint='todos/1',
-    #     response_check=lambda resp: 'title' in resp.json(),
-    #     poke_interval=5,
-    #     timeout=20
-    # )
+with DAG('forex_data_pipeline', start_date=datetime(2022, 8, 21),    schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
+    is_forex_rates_available = HttpSensor(
+        task_id='is_forex_rates_available',
+        http_conn_id='forex_api',
+        # endpoint='marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b',
+        endpoint='todos/1',
+        response_check=lambda resp: 'title' in resp.json(),
+        poke_interval=5,
+        timeout=20
+    )
 
     is_forex_file_available = FileSensor(
         task_id='is_forex_file_available',
@@ -30,4 +59,9 @@ with DAG('forex_data_pipeline', start_date=datetime(2022, 8, 21),           sche
         filepath='forex_currencies.csv',
         poke_interval=5,
         timeout=20
+    )
+
+    is_download_successful = PythonOperator(
+        task_id='is_download_successful',
+        python_callable=download_rates,
     )
